edd2fecafb48d7baa7ffed4612871677b246e44a
mouseart <fengl@funland.cn>
2017-10-05 21:31:12 -0500

    for test

---
+++ _23.h
@@ -0,0 +1,13 @@
+//
+//  _23.h
+//  123
+//
+//  Created by Feng Leon on 13-11-17.
+//  Copyright (c) 2013年 Feng Leon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface _23 : NSObject
+
+@end
---
+++ _23.m
@@ -0,0 +1,13 @@
+//
+//  _23.m
+//  123
+//
+//  Created by Feng Leon on 13-11-17.
+//  Copyright (c) 2013年 Feng Leon. All rights reserved.
+//
+
+#import "_23.h"
+
+@implementation _23
+
+@end
